196710361cec3bb258f9754919c962bd154d85c9
Shayna Jamieson <jamieson.shayna@gmail.com>
2019-10-09 14:33:46 -0700

    Add validation and styles

---
+++ confirmation.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Poppa's Pizza</title>
+</head>
+<body>
+
+    <h1>Thank you for your order!</h1>
+    <h2>Order Summary</h2>
+
+    <pre>
+<!--        puts all data into an array called post -- we print below-->
+        <?php
+            print_r($_POST);
+        ?>
+    </pre>
+</body>
+</html>
